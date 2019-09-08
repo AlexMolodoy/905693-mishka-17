d4744d4b9afbee8e820af3a70d9c7e506d4c7713
alex_molodoy <sivtsov_a@inbox.ru>
2019-09-08 22:16:15 +0300

    okolo final

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var posthtml = require("gulp-posthtml");
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -45,55 +46,56 @@ gulp.task("webp", function () {
 
 gulp.task("html", function () {
   return gulp.src("source/*.html")
-  .pipe(gulp.dest("source"));
+  .pipe(gulp.dest("build"));
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
-
-  gulp.task("copy", function () {
-    return gulp.src([
-      "source/fonts/**/*.{woff,woff2}",
-      "source/img/**",
-      "source/js/**",
-    ], {
-    base: "source"
-    })
-    .pipe(gulp.dest("build"));
-  });
-
-  gulp.task("clean", function () {
-    return del("build");
-  });
-
-  gulp.task("build", gulp.series (
-    "clean",
-    "copy",
-    "css",
-    "html"
-  ));
-
-
-  gulp.task("serve", function () {
-    server.init({
-      server: "build/"
-    });
-
-    gulp.watch("source/less/**/*.less", gulp.series("css"));
-    gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-    gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  });
-
-  gulp.task("refresh", function (done) {
-    server.reload();
-    done();
-  });
-
-  gulp.task("start", gulp.series("build", "server"))
 });
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+  ], {
+  base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("build", gulp.series (
+  "clean",
+  "copy",
+  "css",
+  "html"
+));
+
+
+gulp.task("serve", function () {
+  server.init({
+    server: "build/"
+  });
+
+  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("start", gulp.series("build", "server"))
+
