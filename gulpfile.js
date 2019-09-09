65040119d86b5da70c7603fad05717abe891088a
alex_molodoy <sivtsov_a@inbox.ru>
2019-09-09 14:58:23 +0300

    some

--- gulpfile.js
+++ gulpfile.js
@@ -66,6 +66,7 @@ gulp.task("copy", function () {
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
+    "source/css/**",
   ], {
   base: "source"
   })
--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,22 @@
 var open_img = document.querySelector(".main-nav__button");
 var list = document.querySelector(".main-nav__list");
-var close_img = document.querySelector(".main-nav__list--close");
+var close_img = document.querySelector(".main-nav__close");
 // var open_basket = document.querySelector(".basket-form__open")
 var prod_but = document.querySelector(".product__button");
 var prod_basket = document.querySelector(".basket-form");
+var nojs = document.querySelector(".nojs");
+nojs.classList.remove("nojs");
 open_img.addEventListener("click", function(evt) {
     evt.preventDefault();
-    list.classList.add("main-nav__list--open");
-    open_img.classList.remove("main-nav__list--open");
-    close_img.classList.add("main-nav__list--open");
+    list.classList.add("main-nav__open");
+    open_img.classList.remove("main-nav__open");
+    close_img.classList.add("main-nav__open");
 });
 close_img.addEventListener("click", function(evt) {
     evt.preventDefault();
-    list.classList.remove("main-nav__list--open");
-    close_img.classList.remove("main-nav__list--open");
-    open_img.classList.add("main-nav__list--open");
+    list.classList.remove("main-nav__open");
+    close_img.classList.remove("main-nav__open");
+    open_img.classList.add("main-nav__open");
 });
 prod_but.addEventListener("click", function(evt) {
   evt.preventDefault();
